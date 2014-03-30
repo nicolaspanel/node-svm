56c47d4a0d4e53e24192f9f8446736cae687cda2
nicolaspanel <nicolas.panel@live.fr>
2014-03-30 09:53:04 -0700

    remove hello world tests

--- src/addon.cc
+++ src/addon.cc
@@ -1,14 +1,9 @@
 #include "common.h"
-#include "hello.h"
-#include "helloAsync.h"
 #include "node-svm/node-svm.h"
 
 extern "C" {
   void InitAll(Handle<Object> exports) {
     v8::HandleScope scope;
-    exports->Set(NanSymbol("hello"), FunctionTemplate::New(Hello)->GetFunction());
-    exports->Set(NanSymbol("helloAsync"), FunctionTemplate::New(HelloAsync)->GetFunction());
-
     NodeSvm::Init(exports);
   }
 }
